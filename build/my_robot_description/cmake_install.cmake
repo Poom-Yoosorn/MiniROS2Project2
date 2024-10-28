6c2867902aaa9cafb1c9996a92d66dc3d2b0d90d
Poom Yoosorn <poom.yoosorn@gmail.com>
2024-10-28 22:13:53 +0700

    Write a Launch file to Start the Robot State Publisher with URDF(XML)

--- build/my_robot_description/cmake_install.cmake
+++ build/my_robot_description/cmake_install.cmake
@@ -43,7 +43,10 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/shared/my_robot_description/" TYPE DIRECTORY FILES "/home/poom/Desktop/ros_ws/src/my_robot_description/urdf")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_robot_description/" TYPE DIRECTORY FILES
+    "/home/poom/Desktop/ros_ws/src/my_robot_description/urdf"
+    "/home/poom/Desktop/ros_ws/src/my_robot_description/launch"
+    )
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
