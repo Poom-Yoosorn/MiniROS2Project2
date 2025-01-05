d804c0ff7f3331a8df780a6ee68bfb81797f2c4d
Poom Yoosorn <poom.yoosorn@gmail.com>
2025-01-05 15:14:11 +0700

    Write a LifeCycleNode in C++

--- build/my_robot_bringup/colcon_command_prefix_build.sh
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -1,2 +1,4 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/poom/Desktop/ros_ws/install/lifecycle_cpp/share/lifecycle_cpp/package.sh"
+. "/home/poom/Desktop/ros_ws/install/lifecycle_py/share/lifecycle_py/package.sh"
 . "/home/poom/Desktop/ros_ws/install/my_robot_description/share/my_robot_description/package.sh"
